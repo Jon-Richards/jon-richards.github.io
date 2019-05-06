aaa96125cf23474faa053fcd94125e1c2800b4fb
Jon <talljon@gmail.com>
2019-05-06 01:54:52 -0400

    Fix About section padding.

--- dev/views/components/about/about.tsx
+++ dev/views/components/about/about.tsx
@@ -4,11 +4,11 @@
  */
 
 import * as React from 'react';
-// import * as styles from './about.scss';
 const CSS = require('./about.scss');
 import { Avatar } from '../avatar';
 import { NBSP, NBHP } from '../../lib/ts/html_entities';
 import { SVGWrapper } from '../svg_wrapper';
+import { LoremIpsum } from '../../../../test/content/lorem_ipsum';
 
 /**
  * Renders the about section.
@@ -19,26 +19,30 @@ export const ABOUT: React.FunctionComponent<{
 }> = ({ name }): JSX.Element => {
     return (
         <div className={CSS['root']} role="region" aria-label="About">
-            <div className={CSS['content']}>
-                <div className={CSS['avatar']}>
-                    <div className={CSS['avatar__component']}>
-                        <Avatar
-                            gravatarHash="92acba29258ba766d65ed96b4697b84f"
-                            imageSize={300}
-                            altText="Jon Richards"
-                        />
+            <div className={CSS['scroller']}>
+                <div className={CSS['content']}>
+                    <div className={CSS['avatar']}>
+                        <div className={CSS['avatar__component']}>
+                            <Avatar
+                                gravatarHash="92acba29258ba766d65ed96b4697b84f"
+                                imageSize={300}
+                                altText="Jon Richards"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className={CSS['intro']}>
-                    <h2 className={CSS['intro__title']}>
-                        Howdy!
-                    </h2>
-                    <p className={CSS['intro__text']}>
-                        I'm Jon, a{ NBSP }front{ NBHP }end{ NBSP }developer based in{ NBSP }Boston.
-                    </p>
+                    <div className={CSS['intro']}>
+                        <h2 className={CSS['intro__title']}>
+                            Howdy!
+                        </h2>
+                        <p className={CSS['intro__text']}>
+                            I'm Jon, a{ NBSP }front{ NBHP }end{ NBSP }developer 
+                            based in{ NBSP }Boston.
+                        </p>
+                    </div>
+                    <LoremIpsum />
                 </div>
             </div>
-            <div className={CSS['down']} aria-idden>
+            <div className={CSS['down']} aria-hidden>
                 <SVGWrapper
                     viewBox="0 0 300 100"
                     width="100%"
