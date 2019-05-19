b03c6b5d6cf0ddf5956b9709fe0f29dc03af4851
Jon <talljon@gmail.com>
2019-05-19 00:47:43 -0400

    Switch small device threshold to shared constant

--- dev/views/lib/ts/device_utils.ts
+++ dev/views/lib/ts/device_utils.ts
@@ -7,14 +7,12 @@ import { SMALL_DEVICE_THRESHOLD } from './constants';
 
 /** Provides helper methods for interacting with the device, e.g. the browser, window, etc. */
 export class DeviceUtils {
-    /** Screen width, in pixels, at which adaptive elements should be toggled for small devices. */
-    private static readonly SMALL_DEVICE_THRESHOLD = 1080;
 
     /** 
      * Checks if the viewport is within SMALL_DEVICE_THRESHOLD.
      * @return true the viewport is with SMALL_DEVICE_THRESHOLD.
      */
     get hasSmallScreen(): boolean {
-        return window.innerWidth <= DeviceUtils.SMALL_DEVICE_THRESHOLD;
+        return window.innerWidth <= SMALL_DEVICE_THRESHOLD;
     }
 }
