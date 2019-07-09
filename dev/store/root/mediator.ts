137cda42befaf448b04b63bfa48b71037eccfcc8
Jon <talljon@gmail.com>
2019-07-08 21:10:40 -0400

    Add mediator to root store and gallery

--- dev/store/root/mediator.ts
+++ dev/store/root/mediator.ts
@@ -3,16 +3,14 @@
  * Handles dependencies for the Root module.
  */
 
+ /** Extneral  */
 export { createStore, combineReducers, applyMiddleware } from 'redux';
-
 import thunkMiddleware from 'redux-thunk';
 export { thunkMiddleware };
-
 export { composeWithDevTools } from 'redux-devtools-extension';
-
 export { portfolioReducer } from '../portfolio';
 
+/** Internal */
 import { REDUCER as APP_REDUCER, Store as AppState } from './reducer';
 export { APP_REDUCER, AppState };
-
 export { create } from './create_store';
--- dev/views/components/gallery/mediator.ts
+++ dev/views/components/gallery/mediator.ts
@@ -3,13 +3,13 @@
  * Mediates dependencies within the Gallery module.
  */
 
+ /** External */
 import * as React from 'react';
 export { React };
-
-export { Gallery } from './gallery/gallery';
-
-import { THUMBNAIL as Thumbnail, ThumbnailShape } from './thumbnail/thumbnail';
-export { Thumbnail, ThumbnailShape };
-
 import { v4 as uuid } from 'uuid';
 export { uuid };
+
+/** Internal */
+export { Gallery } from './gallery/gallery';
+import { THUMBNAIL as Thumbnail, ThumbnailShape } from './thumbnail/thumbnail';
+export { Thumbnail, ThumbnailShape };
