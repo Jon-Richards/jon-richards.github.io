ab84314aea3a68e867cafa00c9f4fd25dd8c5348
Jon Richards <talljon@gmail.com>
2022-01-04 20:08:18 -0500

    sass-builder: Strip comments.

--- scripts/build_sass.ts
+++ scripts/build_sass.ts
@@ -4,11 +4,13 @@ import fs from 'fs';
 import chalk from 'chalk';
 
 const newLinesAndWhitespace = /[\r\n\s]+/g;
+const comments = /\/\*(.|[\r\n])*?\*\//g;
 
 const compiled = sass.compile(
     path.resolve(__dirname, '..', 'src', 'scss', 'app.scss'));
 
-const minified = compiled.css.replace(newLinesAndWhitespace, '');
+const withoutComments = compiled.css.replace(comments, '');
+const minified = withoutComments.replace(newLinesAndWhitespace, '');
 
 fs.mkdir(
   path.resolve(__dirname, '..', 'public', 'assets', 'css'),
