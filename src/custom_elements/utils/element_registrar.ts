d7b3750db50156412152b554b253faea0dc1bf40
Jon Richards <talljon@gmail.com>
2022-01-22 19:50:34 -0500

    custom-elements: Comment factory code.

--- src/custom_elements/utils/element_registrar.ts
+++ src/custom_elements/utils/element_registrar.ts
@@ -1,4 +1,10 @@
 import { ElementOptions } from './element_options';
 
+/**
+ * A function that registers a custom element with the browser's custom
+ * element registry.  The options passed to the element being registered should
+ * only be those needed at initialization time, and that cannot be applied at
+ * build time.
+ */
 export type ElementRegistrar = (options: ElementOptions) => void;
 
